cf28866b8147734e211b641970b686ea6f391033
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-29 17:17:50 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,17 +1,17 @@
-class Author 
-  attr_accessor :name, :post 
-  
+class Author
+  attr_accessor :name
+
   @@all = []
 
   def initialize(name)
     @name = name
     @@all << self
   end
-  
+
   def self.all
-    @@all 
-  end 
-  
+    @@all
+  end
+
   def add_post(post)
     post.author = self
   end
