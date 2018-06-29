a0e0e036e6758b5a24f7ccb8df3ad64d58e8525b
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-29 17:15:19 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -12,9 +12,23 @@ class Author
     @@all 
   end 
   
-  def posts (post)
-    @post = post 
-  end 
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+
+  def self.post_count
+    Post.all.count
+  end
   
+end
   
 end 
